4bbf407ae830b29f98ebd280b26095909812cd9d
wuttichaikotwit14-hue <wuttichai.kotwit14@gmail.com>
2026-06-28 18:46:39 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <div style={{ padding: 20 }}>
+    <div style={{ padding: 20, fontFamily: "sans-serif" }}>
       <h1>🚀 Kuean Order</h1>
       <p>เว็บทำงานแล้ว</p>
     </div>
