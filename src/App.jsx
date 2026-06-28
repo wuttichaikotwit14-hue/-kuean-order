fd55802cff1e1f4a9b4a7d940e8ecaed60f0d890
wuttichaikotwit14-hue <wuttichai.kotwit14@gmail.com>
2026-06-28 18:00:21 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 20, fontFamily: "sans-serif" }}>
+      <h1>🚀 Kuean Order</h1>
+      <p>Deploy สำเร็จแล้ว!</p>
+    </div>
+  );
+}
